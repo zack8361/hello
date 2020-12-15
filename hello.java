ac4ae9ea50929fec4f43def18390a92468a05b62
zack8361 <zack6353@naver.com>
2020-12-15 09:52:55 +0000

    Second Commit

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
+      System.out.println("Changes after First commit!");
    }
 }
