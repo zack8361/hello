841f919933555d290e13d74b26e87cd0801a177d
zack8361 <zack6353@naver.com>
2020-12-15 12:27:24 +0000

    Fourth commit

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
       System.out.println("Changes after First commit!");
+      System.out.println("what the fuxk");
    }
 }
+
